745bae0007df9f3ab1055c4dadff39ca336de2e2
wapdev <80967041+thewapdev@users.noreply.github.com>
2021-04-03 22:24:10 -0300

    testnet app

---
+++ static/js/7.abe9f864.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[7],{1122:function(n,i){}}]);
+//# sourceMappingURL=7.abe9f864.chunk.js.map
---
+++ static/js/8.774b525a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[8],{1278:function(n,i){}}]);
+//# sourceMappingURL=8.774b525a.chunk.js.map
